645c6972cba7e6173cab0e3a46a9549a170caf29
Budgo <Budgo@mail.ru>
2022-09-17 20:41:26 +0300

    Исправил баги, сбилдил проект

--- SystemProgramming/Assets/Scripts/Serv/Server.cs
+++ SystemProgramming/Assets/Scripts/Serv/Server.cs
@@ -1,8 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Text;
-using UnityEditor.PackageManager;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Networking;
 
