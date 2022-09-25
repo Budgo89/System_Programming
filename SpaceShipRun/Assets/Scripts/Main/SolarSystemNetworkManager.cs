5751d7a15e7ff1e0c43b8d670b1b6a4bce48004b
Budgo <Budgo@mail.ru>
2022-09-25 13:51:43 +0300

    Задание 1 добавить поле ввода

--- SpaceShipRun/Assets/Scripts/Main/SolarSystemNetworkManager.cs
+++ SpaceShipRun/Assets/Scripts/Main/SolarSystemNetworkManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Characters;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -8,13 +9,53 @@ namespace Main
     {
         [SerializeField] private string playerName;
 
+        Dictionary<int, ShipController> _players = new Dictionary<int, ShipController>();
+
         public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
         {
             var spawnTransform = GetStartPosition();
 
             var player = Instantiate(playerPrefab, spawnTransform.position, spawnTransform.rotation);
-            player.GetComponent<ShipController>().PlayerName = playerName;
+            //player.GetComponent<ShipController>().PlayerName = playerName;
+            _players.Add(conn.connectionId, player.GetComponent<ShipController>());
+
             NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        }
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+            NetworkServer.RegisterHandler(100, ReceiveName);
+        }
+
+        public class MassageLogin : MessageBase
+        {
+            public string login;
+
+            public override void Deserialize(NetworkReader reader)
+            {
+                login = reader.ReadString();
+            }
+
+            public override void Serialize(NetworkWriter writer)
+            {
+                writer.Write(login);
+            }
+        }
+
+        public override void OnClientConnect(NetworkConnection connection)
+        {
+            base.OnClientConnect(connection);
+            MassageLogin _login = new MassageLogin();
+            _login.login = playerName;
+            connection.Send(100, _login);
+        }
+
+        public void ReceiveName(NetworkMessage networkMessage)
+        {
+            _players[networkMessage.conn.connectionId].PlayerName = networkMessage.reader.ReadString();
+            _players[networkMessage.conn.connectionId].gameObject.name =
+                _players[networkMessage.conn.connectionId].PlayerName;
         }
     }
 }
